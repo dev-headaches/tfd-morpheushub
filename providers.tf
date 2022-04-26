40ed7317cae3bab4dff25f56ca1abe92f6cc7d3b
nbeam85 <92865313+nbeam85@users.noreply.github.com>
2022-04-26 12:16:57 -0400

    added provider block

--- providers.tf
+++ providers.tf
@@ -1,16 +1,14 @@
-# for morpheus
-/*
-provider "azurerm" {
-  environment     = "usgovernment"
-  tenant_id       = var.hubtenantid
-  subscription_id = var.hubsubscriptionID
-  client_id       = var.hubclientid
-  client_secret   = var.hubclientsecret
-  features {}
+ terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      version = "~> 2.92"
+      source  = "hashicorp/azurerm"
+    }
+  }
 }
-*/
-
-provider "azurerm" {
+ 
+ provider "azurerm" {
   environment     = var.ARM_ENVIRONMENT
   tenant_id       = var.ARM_TENANT_ID
   subscription_id = var.ARM_SUBSCRIPTION_ID
